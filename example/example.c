6a43e73bb97dd294afec92aa0297551ffded4510
Richard <5922568+riicchhaarrd@users.noreply.github.com>
2024-01-14 11:28:00 +0100

    visitor changes

--- example/example.c
+++ example/example.c
@@ -1,17 +1,52 @@
-
 #include <stdint.h>
+typedef uint8_t uint8;
+typedef uint16_t uint16;
 typedef uint32_t uint32;
+typedef uint64_t uint64;
+typedef int16_t int8;
+typedef int16_t int16;
 typedef int32_t int32;
+typedef int64_t int64;
+typedef float float32;
+typedef double float64;
+typedef char* string;
+typedef uint64_t hash;
 typedef struct
 {
 	float x, y, z;
+	int32_t padding; //aligned
 } vec3;
+typedef struct
+{
+	float x, y, z, w;
+} vec4;
+typedef struct
+{
+	float w, x, y, z;
+} quat;
+typedef struct
+{
+	float m[16];
+} mat4;
+typedef struct
+{
+	float m[9];
+} mat3;
+typedef struct
+{
+	float x, y;
+} vec2;
 
 #include "test.type.h"
 
-int visitor(void *ctx, const char *key, void *value)
+int visit_float(void *ctx, const char *key, float *f)
 {
-	printf("%s = %d\n", key ? key : "", *(int32_t*)value);
+	printf("%s = %f\n", key ? key : "", *f);
+	return 0;
+}
+int visit_(void *ctx, const char *key, void *value, size_t size)
+{
+	printf("%s = %02X\n", key ? key : "", value);
 	return 0;
 }
 
@@ -38,9 +73,12 @@ int main()
 	#else
 	deserialize(fp, &p);
 	#endif
-	printf("health=%d,origin=%f,%f,%f\n",p.health,p.origin.x,p.origin.y,p.origin.z);
+	//printf("health=%d,origin=%f,%f,%f\n",p.health,p.origin.x,p.origin.y,p.origin.z);
+	TypeFieldVisitor visitor = {0};
+	visitor.visit = visit_;
+	visitor.visit_float = visit_float;
 	
-	//visit(&p, visitor, NULL);
+	visit(&p, &visitor, NULL);
 	fclose(fp);
 	return 0;
 }
--- parse/type.h
+++ parse/type.h
@@ -87,10 +87,26 @@ typedef struct TypeEntry_s
 	s32 type_index;
 } TypeEntry;
 
+/*
+Too much work for now, maybe later
+*/
+/*
+typedef struct
+{
+	k_EDataTypeInt,
+	k_EDataTypeFloat,
+	k_EDataTypeString,
+	k_EDataTypeHash
+} k_EDataType;
+*/
+
 typedef struct DataType_s
 {
 	const char *name;
 	u64 hash;
+	//k_EDataType type;
+	//s32 bit_count; //e.g vec3 (float) is 32 bits, 0 means compiler/architecture specific
+	//s32 element_count; //e.g vec3 has 3 elements, default 1, -1 variable length
 	void (*serialize)(struct DataType_s *dt, TypeEntry *entry, Field *field);
 	void (*deserialize)(struct DataType_s *dt, TypeEntry *entry, Field *field);
 	void (*definition)(struct DataType_s *dt, TypeEntry *entry, Field *field);
@@ -442,16 +458,29 @@ void write_serialize_entry(TypeEntry *it)
 		printf("\tinst->type_ = %d;\n", it->index);
 		printf("}\n");
 		fields = it->fields;
-		printf("int vt_fn_visit_type_%d_(void *data, int (*visit_field)(void *ctx, const char *key, void *value, k_EPrimitiveType field_type), void *ctx)\n{\n", it->index);
+		//printf("int vt_fn_visit_type_%d_(void *data, int (*visit_field)(void *ctx, const char *key, void *value, k_EPrimitiveType field_type), void *ctx)\n{\n", it->index);
+		printf("int vt_fn_visit_type_%d_(void *data, TypeFieldVisitor *visitor, void *ctx)\n{\n", it->index);
 		printf("\t%s *inst = (%s*)data;\n\tint changed_count = 0;\n", it->name, it->name);
 		while(fields)
 		{
 			s32 dt_index = data_type_index_by_hash(fields->type_hash);
-			if(it->visibility == k_ETypeEntryVisibilityPrivate)
+			if(dt_index != -1)
 			{
-				printf("\tchanged_count += visit_field(ctx, NULL, &inst->%s, %d);\n", fields->name, dt_index);
-			} else {
-				printf("\tchanged_count += visit_field(ctx, \"%s\", &inst->%s, %d);\n", fields->name, fields->name, dt_index);
+				printf("\tif(visitor->visit_%s)\n\t{\n", data_types[dt_index].name);
+				if(it->visibility == k_ETypeEntryVisibilityPrivate)
+				{
+					printf("\t\tchanged_count += visitor->visit_%s(ctx, NULL, (%s*)&inst->%s);\n", data_types[dt_index].name, data_types[dt_index].name, fields->name);
+				} else {
+					printf("\t\tchanged_count += visitor->visit_%s(ctx, \"%s\", (%s*)&inst->%s);\n", data_types[dt_index].name, fields->name, data_types[dt_index].name, fields->name);
+				}
+				printf("\t}\n\telse if(visitor->visit)\n\t{\n");
+				if(it->visibility == k_ETypeEntryVisibilityPrivate)
+				{
+					printf("\t\tchanged_count += visitor->visit(ctx, NULL, (void*)&inst->%s, sizeof(%s));\n", fields->name, data_types[dt_index].name);
+				} else {
+					printf("\t\tchanged_count += visitor->visit(ctx, \"%s\", (void*)&inst->%s, sizeof(%s));\n", fields->name, fields->name, data_types[dt_index].name);
+				}
+				printf("\t}\n");
 			}
 			fields = fields->next;
 		}
@@ -462,11 +491,19 @@ void write_serialize_entry(TypeEntry *it)
 void write_serialize_table(TypeEntry *entries)
 {
 printf(R"(
+
+typedef void (*SerializeFn)(void *instance, FILE *fp, int writing);
+//typedef int (*VisitFn)(void *instance, int (*visit_field)(void *ctx, const char *key, void *value), void *ctx);
+typedef int (*VisitFn)(void *instance, TypeFieldVisitor *visitor, void *ctx);
+typedef void (*InitializeFn)(void *instance);
 typedef struct
 {
-	void (*serialize)(void *instance, FILE *fp, int writing);
-	int (*visit)(void *instance, int (*visit_field)(void *ctx, const char *key, void *value), void *ctx);
-	void (*initialize)(void *instance);
+	SerializeFn serialize;
+	VisitFn visit;
+	InitializeFn initialize;
+	//void (*serialize)(void *instance, FILE *fp, int writing);
+	//int (*visit)(void *instance, int (*visit_field)(void *ctx, const char *key, void *value), void *ctx);
+	//void (*initialize)(void *instance);
 	size_t size;
 } VTableEntry;	
 )");
@@ -475,7 +512,7 @@ typedef struct
 	{
 		if(entry_type_is_structure(entries->entry_type))
 		{
-			printf("\t{(void*)vt_fn_serialize_type_%d_, (void*)vt_fn_visit_type_%d_, (void*)vt_fn_initialize_type_%d_, sizeof(%s)},\n", entries->index, entries->index, entries->index, entries->name);
+			printf("\t{(SerializeFn)vt_fn_serialize_type_%d_, (VisitFn)vt_fn_visit_type_%d_, (InitializeFn)vt_fn_initialize_type_%d_, sizeof(%s)},\n", entries->index, entries->index, entries->index, entries->name);
 		} else {
 			printf("\t{0, 0},\n");
 		}
@@ -493,10 +530,11 @@ void deserialize(FILE *fp, void *data)
 	int32_t type_index = *(int32_t*)data;
 	vtable_entries[type_index].serialize(data, fp, 0);
 }
-int visit(void *data, int (*visit_field)(void *ctx, const char *key, void *value), void *ctx)
+//int visit(void *data, int (*visit_field)(void *ctx, const char *key, void *value), void *ctx)
+int visit(void *data, TypeFieldVisitor *visitor, void *ctx)
 {
 	int32_t type_index = *(int32_t*)data;
-	return vtable_entries[type_index].visit(data, visit_field, ctx);
+	return vtable_entries[type_index].visit(data, visitor, ctx);
 }
 size_t type_sizeof(int32_t type_index)
 {
@@ -630,6 +668,16 @@ void parse_type_file(const char *path, Arena *arena, CompilerOptions *opts)
 			printf("} k_EType;\n");
 		}
 		{
+			printf("typedef struct\n{\n");
+			for(s32 i = 0; data_types[i].name; ++i)
+			{
+				printf("\tint (*visit_%s)(void *ctx, const char *key, %s *value);\n", data_types[i].name, data_types[i].name);
+			}
+			printf("\tint (*visit)(void *ctx, const char *key, void *value, size_t size); // Fallback in case visit_X is NULL\n");
+			printf("} TypeFieldVisitor;\n");
+		}
+		#if 0
+		{
 			printf("typedef enum\n{\n");
 			for(s32 i = 0; data_types[i].name; ++i)
 			{
@@ -637,6 +685,7 @@ void parse_type_file(const char *path, Arena *arena, CompilerOptions *opts)
 			}
 			printf("} k_EPrimitiveType;\n");
 		}
+		#endif
 		{
 			TypeEntry *it = entries;
 
